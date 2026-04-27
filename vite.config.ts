5e0fce5ce514329855e67eb17a2ebe80a5061694
Ousbren <144965953+Ousbren@users.noreply.github.com>
2026-04-26 22:06:31 -0600

    Limpieza definitiva de submodulos para arreglar el 404

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  // Aseguramos que la base sea el nombre de tu repositorio para GitHub Pages
   base: '/clinica-el-guarco/',
 })
